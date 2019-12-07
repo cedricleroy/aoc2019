db72822c3ff64009c7f548022f86fd5a880ed0a6
Cedric Leroy <cedie73@hotmail.fr>
2019-12-07 16:41:19 -0600

    Day2 Part2

--- day2/src/main.rs
+++ day2/src/main.rs
@@ -2,12 +2,9 @@ use std::fs;
 
 fn int_code(values: &mut Vec<i32>) {
     let len = values.len();
-    println!("Length -> {}", len);
     let mut i = 0;
     loop {
-        println!("Index -> {}", i);
         if i > len {
-            println!("index is out of range: {}", i);
             break;
         }
         match values[i] {
@@ -22,7 +19,6 @@ fn int_code(values: &mut Vec<i32>) {
                 i += 4;
             },
             99 => {
-                println!("Got 99, finishing");
                 break;
             },
             _ => {
@@ -36,13 +32,31 @@ fn main() {
     let text = fs::read_to_string("inputs.txt").expect("got an error opening the file");
     let text = text.trim();
     println!("{}", text);
-    let mut values: Vec<i32> = text.split(",")
+    let original: Vec<i32> = text.split(",")
         .map(|x| x.parse().unwrap())
         .collect();
+    // part 1
+    let mut values = original.clone();
     println!("{:?}", values);
     values[1] = 12;
     values[2] = 2;
     int_code(&mut values);
     println!("{:?}", values);
     assert_eq!(values[0], 5290681);
+    // part 2
+    let mut result: Option<i32> = None;
+    'main: for i in 0..99 {
+        for j in 0..99 {
+            let mut values = original.clone();
+            values[1] = i;
+            values[2] = j;
+            int_code(&mut values);
+            if values[0] == 19690720 {
+                result = Some(100 * i + j);
+                println!("Noun={} Verb={} Result={}", i, j, result.unwrap());
+                break 'main;
+            }
+        }
+    }
+    assert_eq!(result.unwrap(), 5741);
 }
